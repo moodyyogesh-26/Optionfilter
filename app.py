db1c9bfc62b5b2ce88c9e5e40ec049b556485509
sthitaprajnayogi-26 <sthitaprajnayogi@gmail.com>
2026-08-18 19:19:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -651,8 +651,8 @@ def display_option_chain(df, access_token):
     with col_f1:
         sc1, sc2, sc3 = st.columns(3)
         filter_metric = sc1.selectbox("Filter View:", options=["%H", "%C", "%L"], index=1, key="filter_metric_select")
-        min_pct_input = sc2.text_input("🔻 Min % (>=):", value="", placeholder="e.g. 90")
-        max_pct_input = sc3.text_input("🔺 Max % (<=):", value="", placeholder="e.g. 140")
+        min_pct_input = sc2.text_input("🔺 Min % :", value="", placeholder="e.g. 90") #(>=)
+        max_pct_input = sc3.text_input("🔻 Max % :", value="", placeholder="e.g. 140") #(<=)
         
     with col_f2:
         min_strike_input = st.text_input("Min Price:", value="1000", placeholder=">= Price")
